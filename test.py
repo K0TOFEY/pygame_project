3f607e41b2d66726f4ccf62318645304337bc944
K0T0FEY <kirillperovo2008@gmail.com>
2025-02-23 23:40:29 +0300

    Добавление новых кнопок в главном меню (Рекорды и пользователь)

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("HELLO MODERATOR ^))))))")
+import pygame
+
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Крест")
+# формирование кадра:
+# команды рисования на холсте
+pygame.draw.rect(screen, (255, 0, 0), (0, 0, 100, 100))
+# смена (отрисовка) кадра:
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+# завершение работы:
+pygame.quit()
