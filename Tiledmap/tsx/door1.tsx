9456b9e44c79ef04a6dbb9af4177f0dab2041474
KoFey <160953579+K0TOFEY@users.noreply.github.com>
2025-02-26 19:10:18 +0300

    Часть нужных файлов

---
+++ Tiledmap/tsx/door1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="door1" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="../../Sprites/door1.png" width="32" height="64"/>
+</tileset>
